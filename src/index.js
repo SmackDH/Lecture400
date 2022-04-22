9827637aa10d0c058ab2e5c737f4293a3097d2c2
Mattias <66011769+SmackDH@users.noreply.github.com>
2022-04-22 13:35:13 +0900

    make logic, finish finished
    https://stackoverflow.com/questions/64012495/how-to-call-a-function-and-change-heading-text-and-color-depending-on-time-of-th

--- src/index.js
+++ src/index.js
@@ -5,3 +5,24 @@
 //Apply the "heading" style in the styles.css
 //Dynamically change the color of the h1 using inline css styles.
 //Morning = red, Afternoon = green, Night = blue.
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Greeting() {
+  const now = new Date().getHours();
+  const [color, greeting] =
+    now < 12
+      ? ["blue", "Good morning!"]
+      : now > 12 && now < 18
+      ? ["green", "Good afternoon"]
+      : ["blue", "Good evening ser"];
+
+  return <h1 style={{ color }}>{greeting}</h1>;
+}
+
+ReactDOM.render(
+  <div>
+    <Greeting />
+  </div>,
+  document.getElementById("root")
+);
